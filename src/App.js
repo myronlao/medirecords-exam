90e72466a94876b36a67e7de58f3efe19da520f0
Myron David Lao <myronlao@gmail.com>
2021-11-19 15:53:21 +0800

    add unit tests, fix warnings, cosmetic changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
 import { useState } from 'react';
 
-export const processDoubleArray = (arr) => {
+export const processToDoubleArray = (input) => {
+  const arr = input.replace(/ /g, '').split(',');
   return arr.reduce((acc, number) => {
+    if (number === '')
+      return acc;
     acc += acc.length > 0 ? ',' : '';
     if (!isNaN(number) && number !== '') {
       acc += parseInt(number) * 2;
@@ -15,30 +18,29 @@ export const processDoubleArray = (arr) => {
 };
 
 function App() {
-  const [inputArray, setInputArray] = useState();
   const [outputArray, setOutputtArray] = useState();
   return (
     <div className="App">
       <div className="containers">
-        <span>Input</span>
+        <span data-testid="input-title">Input</span>
         Array
         <input
           id="input"
-          value={inputArray}
+          data-testid="input-input"
+          autoFocus
           onChange={(e) => {
             const inputArr = e.target.value.replace(/ /g, '');
-            setInputArray(inputArr);
-            const arrSplit = inputArr.split(',');
-            setOutputtArray(processDoubleArray(arrSplit));
+            setOutputtArray(processToDoubleArray(inputArr));
           }}
         />
       </div>
-      <div className="containers">
-        <span>Output</span>
+      <div className="containers" data-testid="output-container">
+        <span data-testid="output-title">Output</span>
         Double
         <input
           id="output"
-          value={outputArray}
+          data-testid="output-input"
+          defaultValue={outputArray}
         />
       </div>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from '@testing-library/react';
+import App, {processToDoubleArray} from './App';
 
-test('renders learn react link', () => {
+test('renders', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByTestId('input-title')).toHaveTextContent('Input');
+
+  expect(screen.getByTestId('output-title')).toHaveTextContent('Output');
+});
+
+test('input on change', () => {
+  const wrapper = render(<App/>);
+  const input = wrapper.getByTestId('input-input');
+  fireEvent.change(input, {target: {value: '23'}});
+  expect(input.value).toBe('23');
+
+  const output = wrapper.getByTestId('output-input');
+  expect(output.value).toBe('46');
+});
+
+test('processDoubleArray', () => {
+  expect(processToDoubleArray('1, 2, 3 ,a, b, 40,500 ,, , ,6000')).toBe('2,4,6,a,b,80,1000,12000');
 });
